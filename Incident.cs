16f60eb033697c87581d1db43dcbf516210351f3
Panut Tavilsup <panut.tavilsup@gmail.com>
2023-08-22 20:04:44 -0600

    Add Incident class and MainWindow functionality.

---
+++ Incident.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncidentReport
+{
+    class Incident
+    {
+        private int incidentID;
+        private DateTime incidentDate;
+        private string projectName, vendorCompanyName, vendorContactName, vendorContactEmail, incidentDescription;
+        private decimal incidentCost;
+
+        public Incident()
+        {
+            incidentID = 0;
+            incidentDate = DateTime.Today;
+            projectName = string.Empty;
+            vendorCompanyName = string.Empty;
+            vendorContactName = string.Empty;
+            vendorContactEmail = string.Empty;
+            incidentCost = 0;
+            incidentDescription = string.Empty;
+        }
+        public Incident(int incidentID, DateTime incidentDate, string projectName,
+                       string vendorCompanyName, string vendorContactName, string vendorContactEmail,
+                       decimal incidentCost, string incidentDescription)
+        {
+            this.incidentID = incidentID;
+            this.incidentDate = incidentDate;
+            this.projectName = projectName;
+            this.vendorCompanyName = vendorCompanyName;
+            this.vendorContactName = vendorContactName;
+            this.vendorContactEmail = vendorContactEmail;
+            this.incidentCost = incidentCost;
+            this.incidentDescription = incidentDescription;
+        }
+        
+        /* IncidentId Field */
+        public int GetIncidentId()
+        {
+            return incidentID;
+        }
+        public void SetIncidentId(int incidentID)
+        {
+            this.incidentID = incidentID;
+        }
+        
+        /* IncidentDate Field */
+        public DateTime GetIncidentDate()
+        {
+            return incidentDate;
+        }
+        public void SetIncidentDate(DateTime incidentDate)
+        {
+            this.incidentDate = incidentDate;
+        }
+        
+        /* ProjectName Field */
+        public string GetProjectName()
+        {
+            return projectName;
+        }
+        public void SetProjectName(string projectName)
+        {
+            this.projectName = projectName;
+        }
+        
+        /* VendorCompanyName Field */
+        public string GetVendorCompanyName()
+        {
+            return vendorCompanyName;
+        }
+        public void SetVendorCompanyName(string vendorCompanyName)
+        {
+            this.vendorCompanyName = vendorCompanyName;
+        }
+        
+        /* VendorContactName Field */
+        public string GetVendorContactName()
+        {
+            return vendorContactName;
+        }
+        public void SetVendorContactName(string vendorContactName)
+        {
+            this.vendorContactName = vendorContactName;
+        }
+        
+        /* VendorContactEmail Field */
+        public string GetVendorContactEmail()
+        {
+            return vendorContactEmail;
+        }
+        public void SetVendorContactEmail(string vendorContactEmail)
+        {
+            this.vendorContactEmail = vendorContactEmail;
+        }
+        
+        /* IncidentCost Field */
+        public decimal GetIncidentCost()
+        {
+            return incidentCost;
+        }
+        public void SetIncidentCost(decimal incidentCost)
+        {
+            this.incidentCost = incidentCost;
+        }
+        
+        /* IncidentDescription Field */
+        public string GetIncidentDescription()
+        {
+            return incidentDescription;
+        }
+        public void SetIncidentDescription(string incidentDescription)
+        {
+            this.incidentDescription = incidentDescription;
+        }
+
+        /* Printing Section */
+        public string IncidentPrintHeader()
+        {
+            return "Incident ID \t Incident Date \t Project Name \t\t Vendor Company Name \t Vendor Contact Name \t Vendor Contact Email \t\t Incident Cost \t Incident Description";
+        }
+        public override string ToString()
+        {
+            return $"{incidentID} \t\t {incidentDate.ToShortDateString()} \t {projectName,-25} \t {vendorCompanyName,-30} \t {vendorContactName,-30} \t {vendorContactEmail,-32} \t {incidentCost,-10:C2} \t {incidentDescription}";
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -15,14 +17,346 @@ using System.Windows.Shapes;
 
 namespace IncidentReport
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Incident> incidentList = new List<Incident>();
+        private Incident currentIncident = new Incident();
+        private int currentIndex;
+        private string projectNameFilter = "";
+
+        /******* IMPORTANT *****
+         * Tutorial videos indicate that relative path of SQL Connection String can not be used.
+         * Please consider changing the SQL Connection String below if this application running path is changed.
+         * String Location: click "ConnectionName".mdf, then copy Connection String from Properties.
+         */
+        //Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename="C:\Users\panut\Desktop\COMP255 (Visual Application Development)\Csharp App\255FPTavilsup\ProjectIncidents.mdf";Integrated Security=True
+        string sqlConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename='C:\Users\panut\Desktop\Git\IncidentReport\ProjectIncidents.mdf';Integrated Security=True";
+        
         public MainWindow()
         {
             InitializeComponent();
+
+            //Initial the application by connecting to the database then download to show all records.
+            LoadDatabase("");
+        }
+
+        private void ListBoxIncidents_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            TextBoxErrorMessage.Text = "";
+
+            currentIndex = ListBoxIncidents.SelectedIndex - 1;                                      //Index 0 of the list box is the data header. 
+            if (currentIndex >= 0)
+            {
+                currentIncident = (Incident)ListBoxIncidents.SelectedItem;                          //Convert the selected item into a class.
+            }
+            
+            DisplayIncidentToTextBoxes();
+        }
+        private void DisplayIncidentToTextBoxes()
+        {
+            if (currentIncident != null)
+            {
+                TextBoxIncidentID.Text = currentIncident.GetIncidentId().ToString();
+                TextBoxIncidentDate.Text = currentIncident.GetIncidentDate().ToShortDateString();
+                TextBoxProjectName.Text = currentIncident.GetProjectName();
+                TextBoxCompanyName.Text = currentIncident.GetVendorCompanyName();
+                TextBoxContactName.Text = currentIncident.GetVendorContactName();
+                TextBoxContactEmail.Text = currentIncident.GetVendorContactEmail();
+                TextBoxIncidentCost.Text = $"{currentIncident.GetIncidentCost(),0:C2}";
+                TextBoxIncidentDescription.Text = currentIncident.GetIncidentDescription();
+            }
+            else
+            {
+                DisplayEmptyIncident();
+            }
+        }
+        private void DisplayEmptyIncident()
+        {
+            TextBoxIncidentID.Text = string.Empty;
+            TextBoxIncidentDate.Text = string.Empty;
+            TextBoxProjectName.Text = string.Empty;
+            TextBoxCompanyName.Text = string.Empty;
+            TextBoxContactName.Text = string.Empty;
+            TextBoxContactEmail.Text = string.Empty;
+            TextBoxIncidentCost.Text = string.Empty;
+            TextBoxIncidentDescription.Text = string.Empty;
+
+            currentIncident = null;                                                                 //Deselect current item.
+        }
+
+        private void ButtonApplyFilter_Click(object sender, RoutedEventArgs e)
+        {
+            TextBoxErrorMessage.Text = "";                                                          //Clear error messages.
+
+            LoadDatabase("");                                                                       //Always load all database before matching with the filter.
+
+            if (TextBoxFilter.Text != "")
+            {
+                Boolean foundRecord = false;
+
+                for (int i = 0; i < incidentList.Count; i++)
+                {
+                    //Seaching Project Name from the downloaded records. Therefore, LoadDatabase("") first is mandatory.
+                    if (incidentList[i].GetProjectName() == TextBoxFilter.Text && foundRecord == false)
+                    {
+                        foundRecord = true;
+                    }
+                }
+
+                if (foundRecord)
+                {
+                    projectNameFilter = TextBoxFilter.Text;                                         //Pass a valid Project Name to be filtered in a database query.
+                    LoadDatabase(projectNameFilter);
+                }
+                else
+                {
+                    DisplayEmptyIncident();
+                    ListBoxIncidents.Items.Clear();
+                    ListBoxIncidents.Items.Add("No matching record!");
+                    TextBoxErrorMessage.Text = "No Project Name matchs the input filter.";
+                    TextBoxTotalCost.Text = string.Empty;
+                }
+            }
+        }
+
+        private void LoadDatabase(string loadOption)                                                //Bioption database loader.
+        {
+            incidentList.Clear();
+            ListBoxIncidents.Items.Clear();
+            string sqlQuery;
+
+            /**
+              * 4 Steps of Database Connection Layers:
+              * 1. Allocate, configure, and open your connection object.
+              * 2. Allocate and configure a command object, specifying the connection object as a constructor argument or with the Connection property.
+              * 3. Call ExecuteReader() on the configured command class.
+              * 4. Process each record using the Read() method of the data reader.
+              */
+            using (SqlConnection connection = new SqlConnection(sqlConnectionString))               //Step 1. Using "using" for a temporary object; to be cleared out after finish.
+            {
+                connection.Open();
+
+                if (loadOption != "")
+                {
+                    sqlQuery = $"SELECT * FROM Incidents WHERE ProjectName = '{loadOption}';";      // 1st Option: download records with a Project Name filter.
+                }
+                else
+                {
+                    sqlQuery = "SELECT * FROM Incidents;";                                          // 2nd Option: download all records.
+                }
+
+                using (SqlCommand command = new SqlCommand(sqlQuery, connection))                   //Step 2.
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())                          //Step 3.
+                    {
+                        Incident newIncident = new Incident();
+                        ListBoxIncidents.Items.Add(newIncident.IncidentPrintHeader());
+
+                        while (reader.Read())                                                       //Step 4.
+                        {
+                            newIncident = new Incident((int)reader["IncidentId"],
+                                                       (DateTime)reader["IncidentDate"],
+                                                       (string)reader["ProjectName"],
+                                                       (string)reader["VendorCompanyName"],
+                                                       (string)reader["VendorContactName"],
+                                                       (string)reader["VendorContactEmail"],
+                                                       (decimal)reader["IncidentCost"],
+                                                       (string)reader["IncidentDescription"]);
+                            
+                            incidentList.Add(newIncident);
+                            ListBoxIncidents.Items.Add(newIncident);
+                        }
+                    }
+                }
+
+                connection.Close();
+            }
+            DisplayTotalCost();
+        }
+
+        private void DisplayTotalCost()
+        {
+            decimal totalCost = 0;
+
+            //Sum the total cost with the current records in the downloaded list only.
+            for (int i=0; i<incidentList.Count; i++)
+            {
+                totalCost += incidentList[i].GetIncidentCost();
+            }
+
+            TextBoxTotalCost.Text = $"{totalCost,0:C2}";
+        }
+
+        private void ButtonSave_Click(object sender, RoutedEventArgs e)
+        {
+            bool validInput = false;
+            validInput = ValidateInputFromTextBoxes(currentIncident);                               //Input validation from the text boxes.
+
+            if (validInput)
+            {
+                using (SqlConnection connection = new SqlConnection(sqlConnectionString))
+                {
+                    connection.Open();
+
+                    string sqlQuery = $"UPDATE Incidents SET " +
+                                      $"IncidentDate = '{currentIncident.GetIncidentDate()}', " +
+                                      $"ProjectName = '{currentIncident.GetProjectName()}', " +
+                                      $"VendorCompanyName = '{currentIncident.GetVendorCompanyName()}', " +
+                                      $"VendorContactName = '{currentIncident.GetVendorContactName()}', " +
+                                      $"VendorContactEmail = '{currentIncident.GetVendorContactEmail()}', " +
+                                      $"IncidentCost = '{currentIncident.GetIncidentCost()}', " +
+                                      $"IncidentDescription = '{currentIncident.GetIncidentDescription()}' " +
+                                      $"WHERE IncidentId = {currentIncident.GetIncidentId()};";     //Update to the specific primary key number.
+
+                    using (SqlCommand Command = new SqlCommand(sqlQuery, connection))
+                    {
+                        Command.ExecuteNonQuery();                                                  //"Update" command execution statement.
+                    }
+
+                    connection.Close();
+                }
+
+                LoadDatabase(projectNameFilter);
+            }
+        }
+
+        private void ButtonSaveNew_Click(object sender, RoutedEventArgs e)
+        {
+            bool validInput = false;
+            validInput = ValidateInputFromTextBoxes(currentIncident);                               //Input validation from the text boxes.
+
+            if (validInput)
+            {
+                using (SqlConnection connection = new SqlConnection(sqlConnectionString))
+                {
+                    connection.Open();
+
+                    /* First query : find the maximum number of the primary key */
+                    string sqlQuery = "SELECT Max(IncidentId) FROM Incidents;";
+
+                    using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                    {
+                        //"Select" command execution returns a first column first row value from the query which is Max(IncidentId);
+                        int newIncidentId = (int)command.ExecuteScalar() + 1;                       //Add 1 for increasing the database by 1 record.
+
+                        currentIncident.SetIncidentId(newIncidentId);                               //Complete an object for INSERT command below.
+                    }
+
+                    /* Second query : insert new data to a new number of the primary key */
+                    sqlQuery = $"INSERT INTO Incidents " +
+                               "(IncidentId, IncidentDate, ProjectName, VendorCompanyName, VendorContactName, VendorContactEmail, IncidentCost, IncidentDescription)" +
+                               "VALUES (" +
+                               $"{currentIncident.GetIncidentId()}, " +
+                               $"'{currentIncident.GetIncidentDate()}', " +
+                               $"'{currentIncident.GetProjectName()}', " +
+                               $"'{currentIncident.GetVendorCompanyName()}', " +
+                               $"'{currentIncident.GetVendorContactName()}', " +
+                               $"'{currentIncident.GetVendorContactEmail()}', " +
+                               $"'{currentIncident.GetIncidentCost()}', " +
+                               $"'{currentIncident.GetIncidentDescription()}');";
+
+                    using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                    {
+                        command.ExecuteNonQuery();                                                  //"Insert" command execution statement.
+                    }
+
+                    connection.Close();
+                }
+
+                LoadDatabase(projectNameFilter);
+                DisplayIncidentToTextBoxes();
+            }
+        }
+
+        private void ButtonDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if(currentIncident != null)
+            {
+                using (SqlConnection connection = new SqlConnection(sqlConnectionString))
+                {
+                    connection.Open();
+
+                    string sqlQuery = $"DELETE FROM Incidents WHERE IncidentId = {currentIncident.GetIncidentId()};";   //Delete at a specific primary key number.
+
+                    using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                    {
+                        command.ExecuteNonQuery();                                                  //"Delete" command execution statement.
+                    }
+
+                    connection.Close();
+                }
+
+                LoadDatabase(projectNameFilter);
+                
+                //Clear all the text boxes and the current incident for not allowing continuously deleting records.
+                DisplayEmptyIncident();
+            }
+        }
+
+        private void TextBoxFilter_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(TextBoxFilter.Text == "")
+            {
+                //Refresh the list box by re-download all the record from the database. This helps user to know Project Names for the next filtering.
+                projectNameFilter = "";
+                LoadDatabase(projectNameFilter);
+                
+                TextBoxErrorMessage.Text = "";
+            }
+        }
+
+        private Boolean ValidateInputFromTextBoxes(Incident incidentObject)
+        {
+            TextBoxErrorMessage.Text = "";
+            decimal incidentCost;
+            string incidentCostText;
+            DateTime incidentDate;
+
+            if (TextBoxIncidentCost.Text == "")
+            {
+                TextBoxErrorMessage.Text = "Invalid Input!";
+                return false;
+            }
+            else
+            {
+                //Selecting only the number; discard the "$" sign.
+                if (TextBoxIncidentCost.Text.Substring(0, 1) == "$")
+                {
+                    incidentCostText = TextBoxIncidentCost.Text.Substring(1);
+                }
+                else
+                {
+                    incidentCostText = TextBoxIncidentCost.Text;
+                }
+
+                if (!DateTime.TryParse(TextBoxIncidentDate.Text, out incidentDate))
+                {
+                    incidentDate = DateTime.Now;
+                }
+
+                //Main data validation.
+                if (TextBoxProjectName.Text != "" &&
+                    TextBoxCompanyName.Text != "" &&
+                    TextBoxContactName.Text != "" &&
+                    TextBoxContactEmail.Text != "" &&
+                    decimal.TryParse(incidentCostText, out incidentCost))
+                {
+                    incidentObject.SetIncidentDate(incidentDate);
+                    incidentObject.SetProjectName(TextBoxProjectName.Text);
+                    incidentObject.SetVendorCompanyName(TextBoxCompanyName.Text);
+                    incidentObject.SetVendorContactName(TextBoxContactName.Text);
+                    incidentObject.SetVendorContactEmail(TextBoxContactEmail.Text);
+                    incidentObject.SetIncidentCost(incidentCost);
+                    incidentObject.SetIncidentDescription(TextBoxIncidentDescription.Text);
+
+                    return true;
+                }
+                else
+                {
+                    TextBoxErrorMessage.Text = "Invalid Input!";
+                    return false;
+                }
+            }
         }
     }
 }
