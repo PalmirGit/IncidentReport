24d88998aa46581a82b0b51bb69fa7ec61b70c17
Panut Tavilsup <panut.tavilsup@gmail.com>
2023-09-26 19:40:52 -0600

    Split functions for clean code style and add function descriptions

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,12 +24,6 @@ namespace IncidentReport
         private Incident currentIncident = new Incident();
         private int currentIndex;
 
-        /******* IMPORTANT *****
-         * Tutorial videos indicate that relative path of SQL Connection String can not be used.
-         * Please consider changing the SQL Connection String below if this application running path is changed.
-         * String Location: click "ConnectionName".mdf, then copy Connection String from Properties.
-         */
-        //Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename="C:\Users\panut\Desktop\COMP255 (Visual Application Development)\Csharp App\255FPTavilsup\ProjectIncidents.mdf";Integrated Security=True
         string sqlConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename='C:\Users\panut\Desktop\Git\IncidentReport\ProjectIncidents.mdf';Integrated Security=True";
         
         public MainWindow()
@@ -40,18 +34,10 @@ namespace IncidentReport
             LoadDatabase();
         }
 
-        private void ListBoxIncidents_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            TextBoxErrorMessage.Text = "";
-
-            currentIndex = ListBoxIncidents.SelectedIndex - 1;                                      //Index 0 of the list box is the data header. 
-            if (currentIndex >= 0)
-            {
-                currentIncident = (Incident)ListBoxIncidents.SelectedItem;                          //Convert the selected item into a class.
-            }
-            
-            DisplayIncidentToTextBoxes();
-        }
+        
+        /*
+         * Display the current incident.
+         */
         private void DisplayIncidentToTextBoxes()
         {
             if (currentIncident != null)
@@ -70,6 +56,26 @@ namespace IncidentReport
                 DisplayEmptyIncident();
             }
         }
+
+        /*
+         * Get index and display the selected incident.
+         */
+        private void ListBoxIncidents_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            TextBoxErrorMessage.Text = "";
+
+            currentIndex = ListBoxIncidents.SelectedIndex - 1;                                      //Index 0 of the list box is the data header. 
+            if (currentIndex >= 0)
+            {
+                currentIncident = (Incident)ListBoxIncidents.SelectedItem;                          //Convert the selected item into a class.
+            }
+
+            DisplayIncidentToTextBoxes();
+        }
+
+        /*
+         * Clear all input boxes.
+         */
         private void DisplayEmptyIncident()
         {
             TextBoxIncidentID.Text = string.Empty;
@@ -84,58 +90,26 @@ namespace IncidentReport
             currentIncident = null;                                                                 //Deselect current item.
         }
 
-        private void ButtonApplyFilter_Click(object sender, RoutedEventArgs e)
+        /*
+         * Display total cost of all the projects in the list box (incident list).
+         */
+        private void DisplayTotalCost(List<Incident> incidentList)
         {
-            TextBoxErrorMessage.Text = "";                                                          //Clear error messages.
+            decimal totalCost = 0;
 
-            LoadDatabase();                                                                       //Always load all database before matching with the filter.
-
-            if (TextBoxFilter.Text != "")
+            foreach (Incident incident in incidentList)
             {
-                Boolean foundRecord = false;
-                char delimiter = ' ';
-                string[] searchList = TextBoxFilter.Text.Split(new[] { delimiter }, StringSplitOptions.RemoveEmptyEntries);
-
-                DisplayEmptyIncident();
-                ListBoxIncidents.Items.Clear();
-                ListBoxIncidents.Items.Add(incidentList[0].IncidentPrintHeader());
-                TextBoxTotalCost.Text = string.Empty;
-                List<Incident> tempList = new List<Incident>();
-
-                try
-                {
-                    foreach (Incident incident in incidentList)
-                    {
-                        //if (searchList.All(word => incident.GetProjectName().Contains(word, StringComparison.OrdinalIgnoreCase)))
-                        if (searchList.Any(word => incident.GetProjectName().Contains(word, StringComparison.OrdinalIgnoreCase)))
-                        {
-                            tempList.Add(incident);
-                            ListBoxIncidents.Items.Add(incident);
-                            foundRecord = true;
-                        }
-                    }
-
-                    if (foundRecord)
-                    {
-                        incidentList = tempList;
-                        DisplayTotalCost();
-                    }
-                }
-                catch (ArgumentException argEx)
-                {
-                    TextBoxErrorMessage.Text = "Invalid regular expression pattern: " + argEx.Message;
-                }
-                catch (Exception ex)
-                {
-                    TextBoxErrorMessage.Text = "An error occurred: " + ex.Message;
-                }
+                totalCost += incident.GetIncidentCost();
             }
+
+            TextBoxTotalCost.Text = $"{totalCost,0:C2}";
         }
 
-        private void LoadDatabase()
+        /*
+         * Refresh the box list from an incident list (originally desiged for the most recent list).
+         */
+        private void ReloadIncidentBoxList(List<Incident> incidentList)
         {
-            incidentList = DataService.ReadIncident(sqlConnectionString);
-
             ListBoxIncidents.Items.Clear();
             ListBoxIncidents.Items.Add(incidentList[0].IncidentPrintHeader());
 
@@ -144,35 +118,58 @@ namespace IncidentReport
                 ListBoxIncidents.Items.Add(incident);
             }
 
-            DisplayTotalCost();
+            DisplayTotalCost(incidentList);
         }
 
-        private void DisplayTotalCost()
+        /*
+         * Filter and display project names with multi-word capability.
+         */
+        private void ButtonApplyFilter_Click(object sender, RoutedEventArgs e)
         {
-            decimal totalCost = 0;
+            TextBoxErrorMessage.Text = "";                                                        //Clear error messages.
 
-            //Sum the total cost with the current records in the downloaded list only.
-            for (int i = 0; i < incidentList.Count; i++)
+            LoadDatabase();                                                                       //Always load all database before matching with the filter.
+
+            if (TextBoxFilter.Text != "")
             {
-                totalCost += incidentList[i].GetIncidentCost();
-            }
+                Boolean foundRecord = false;
+                char delimiter = ' ';
+                string[] searchList = TextBoxFilter.Text.Split(new[] { delimiter }, StringSplitOptions.RemoveEmptyEntries);     //Split seaching words into a list.
 
-            TextBoxTotalCost.Text = $"{totalCost,0:C2}";
+                DisplayEmptyIncident();
+                
+                TextBoxTotalCost.Text = string.Empty;
+                List<Incident> tempList = new List<Incident>();
+
+                try
+                {
+                    foreach (Incident incident in incidentList)
+                    {
+                        if (searchList.All(word => incident.GetProjectName().Contains(word, StringComparison.OrdinalIgnoreCase)))
+                        //if (searchList.Any(word => incident.GetProjectName().Contains(word, StringComparison.OrdinalIgnoreCase)))
+                        {
+                            tempList.Add(incident);
+                            foundRecord = true;
+                        }
+                    }
+
+                    if (foundRecord)
+                    {
+                        incidentList = tempList;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    TextBoxErrorMessage.Text = "An error occurred: " + ex.Message;
+                }
+
+                ReloadIncidentBoxList(incidentList);
+            }
         }
 
-        private void ButtonUpdate_Click(object sender, RoutedEventArgs e)
-        {
-            bool validInput = false;
-            validInput = ValidateInputFromTextBoxes(currentIncident);                               //Input validation from the text boxes.
-
-            if (validInput)
-            {
-                DataService.UpdateIncident(sqlConnectionString, currentIncident);
-
-                LoadDatabase();                                                                     //Reflesh the data
-            }
-        }
-
+        /*
+         * Database (Create), add a new incident.
+         */
         private void ButtonSaveNew_Click(object sender, RoutedEventArgs e)
         {
             bool validInput = false;
@@ -187,6 +184,35 @@ namespace IncidentReport
             }
         }
 
+        /*
+         * Database (Read), load all incidents.
+         */
+        private void LoadDatabase()
+        {
+            incidentList = DataService.ReadIncident(sqlConnectionString);
+
+            ReloadIncidentBoxList(incidentList);                                                  //Every time database is reloaded, the list box should be reloaded also.
+        }
+
+        /*
+         * Database (Update), update an incident.
+         */
+        private void ButtonUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            bool validInput = false;
+            validInput = ValidateInputFromTextBoxes(currentIncident);                               //Input validation from the text boxes.
+
+            if (validInput)
+            {
+                DataService.UpdateIncident(sqlConnectionString, currentIncident);
+
+                LoadDatabase();                                                                     //Reflesh the data
+            }
+        }
+        
+        /*
+         * Database (Delete), delete an incident.
+         */
         private void ButtonDelete_Click(object sender, RoutedEventArgs e)
         {
             if(currentIncident != null)
@@ -200,16 +226,22 @@ namespace IncidentReport
             }
         }
 
+        /*
+         * Reflesh the data and boxlist when filter is no longer applied.
+         */
         private void TextBoxFilter_TextChanged(object sender, TextChangedEventArgs e)
         {
             if(TextBoxFilter.Text == "")
             {
-                LoadDatabase();                                                                     //Reflesh the data
-
-                TextBoxErrorMessage.Text = "";
+                LoadDatabase();
+                TextBoxErrorMessage.Text = "";                                                      //Clear messages in the error text box.
             }
         }
 
+        /*
+         * Validate input boxes.
+         * Output: validation result as true or false.
+         */
         private Boolean ValidateInputFromTextBoxes(Incident incidentObject)
         {
             TextBoxErrorMessage.Text = "";
@@ -224,7 +256,7 @@ namespace IncidentReport
             }
             else
             {
-                //Selecting only the number; discard the "$" sign.
+                //Selecting only the cost number; discard the "$" sign.
                 if (TextBoxIncidentCost.Text.Substring(0, 1) == "$")
                 {
                     incidentCostText = TextBoxIncidentCost.Text.Substring(1);
@@ -234,6 +266,7 @@ namespace IncidentReport
                     incidentCostText = TextBoxIncidentCost.Text;
                 }
 
+                //Use current time if date time is not provided or in an incorrect format.
                 if (!DateTime.TryParse(TextBoxIncidentDate.Text, out incidentDate))
                 {
                     incidentDate = DateTime.Now;
